39cd861336047c072da8ea8164a9e73c096fd36c
Prasad Kulkarni <prasadkulkarni2027@gmail.com>
2026-01-17 00:05:18 +0530

    16 Jan - added separation of concerns in models and created owner model

---
+++ models/product-model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+const productSchema =mongoose.Schema({
+image:String,
+name:String,
+
+discount :{
+    type: Number,
+    default:0
+},
+bgcolor : String,
+panelcolor :String,
+textcolor: String,
+price:Number,
+});
+
+module.exports = mongoose.model("product",productSchema);
